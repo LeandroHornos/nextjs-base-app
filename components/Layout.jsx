400d40b503ca520c823531da2f647e52d57b5855
Leandro Hornos <leandrohornos@gmail.com>
2021-11-02 13:26:48 -0300

    creo componente de layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,3 +1,7 @@
+import { Fragment } from "react";
+
+import Head from "next/head";
+
 export const FlexRowLg10 = (props) => {
   {
     /*Este componente de */
@@ -23,5 +27,36 @@ export const FlexRow3ColLg = (props) => {
       </div>
       <div className={"col-lg-" + props.col3 + " " + props.col3Classes}></div>
     </div>
+  );
+};
+
+export const GeneralLayout = (props) => {
+  const { children } = props;
+
+  return (
+    <Fragment>
+      <Head>
+        <title>My Camion</title>
+        <meta
+          name="description"
+          content="Aplicación web para la gestión de un flete entre dos ubicaciones"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+      <footer>
+        <div className="row min20">
+          <div className="col-12 d-flex flex-column justify-content-center align-items-center">
+            <a
+              href="https://www.leandrohornos.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              created by Leandro Hornos
+            </a>
+          </div>
+        </div>
+      </footer>
+    </Fragment>
   );
 };
