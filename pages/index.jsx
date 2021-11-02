b1a83696a7f7cbb97c04b36aa903783d896dab67
Leandro Hornos <leandrohornos@gmail.com>
2021-11-02 16:26:54 -0300

    Agrego BlankLayout al index

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,35 +8,27 @@ export default function LandingPage() {
   const router = useRouter();
   return (
     <BlankLayout>
-      <main>
-        <div className="row min80">
-          <div className="col-sm-2 col-lg-4"></div>
-          <div className="col-sm-8 col-lg-4 d-flex flex-column justify-content-center align-items-center">
-            <header>
-              <h1>Hola! esto es</h1>
-              <h1>My Camion</h1>
-              <p>
-                <strong>
-                  <small>Gestiona envios entre locaciones</small>
-                </strong>
-              </p>
-            </header>
-            <div class="d-grid width100 gap-3">
-              <Link href="/login">
-                <button className="btn btn-primary" type="button">
-                  Entrar
-                </button>
-              </Link>
-              <Link href="/register">
-                <button className="btn btn-dark" type="button">
-                  Registro
-                </button>
-              </Link>
-            </div>
-          </div>
-          <div className="col-sm-2 col-lg-4"></div>
-        </div>
-      </main>
+      <header>
+        <h1>Hola! esto es</h1>
+        <h1>My Camion</h1>
+        <p>
+          <strong>
+            <small>Gestiona envios entre locaciones</small>
+          </strong>
+        </p>
+      </header>
+      <div class="d-grid width100 gap-3">
+        <Link href="/login">
+          <button className="btn btn-primary" type="button">
+            Entrar
+          </button>
+        </Link>
+        <Link href="/register">
+          <button className="btn btn-dark" type="button">
+            Registro
+          </button>
+        </Link>
+      </div>
     </BlankLayout>
   );
 }
