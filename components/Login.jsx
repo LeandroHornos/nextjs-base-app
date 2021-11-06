48976c710d75486b64b37f9c97f4feee4f19931b
Leandro Hornos <leandrohornos@gmail.com>
2021-11-06 12:04:58 -0300

    Paso login a landingpage

---
+++ components/Login.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+
+// Componentes
+import { InputErrorMessage } from "../components/FormComponents";
+
+// Reglas de validación del formulario
+
+const val = {
+  email: {
+    rules: {
+      pattern: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+      required: true,
+    },
+    msgs: {
+      pattern: "El formato del email ingresado no es válido",
+      required: "Debes ingresar tu email",
+    },
+  },
+  password: {
+    rules: {
+      required: true,
+    },
+    msgs: {
+      required: "Debes ingresar tu password",
+    },
+  },
+};
+
+export default function Login() {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("esta es la data", data);
+  };
+  return (
+    <div className="shadowed-box box-bg">
+      <h1>Ingresar</h1>
+      <form className="width100" onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label className="form-label">Email</label>
+          <input
+            type="email"
+            placeholder="yourname@company.com"
+            className="form-control"
+            {...register("email", { ...val.email.rules })}
+            aria-describedby="email"
+          />
+          <InputErrorMessage>
+            {errors.email ? val.email.msgs[errors.email.type] : ""}
+          </InputErrorMessage>
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            {...register("password", { ...val.password.rules })}
+          />
+          {errors.password && (
+            <InputErrorMessage>
+              {val.password.msgs[errors.password.type]}
+            </InputErrorMessage>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Entrar
+        </button>
+      </form>
+      <button
+        className="btn btn-link"
+        onClick={(e) => {
+          e.preventDefault();
+          router.push("/recover");
+        }}
+      >
+        Olvidé mi password
+      </button>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+import Login from "../components/Login";
+
 import { BlankLayout } from "../components/Layout";
 
 export default function LandingPage() {
@@ -8,31 +10,33 @@ export default function LandingPage() {
   return (
     <BlankLayout>
       <div className="row">
-        <div className="col-sm-2 col-md-3 col-lg-4"></div>
-        <div className="col-sm-8 col-md-6 col-lg-4">
-          <header>
-            <h1>Hola! esto es</h1>
-            <h1>My Camion</h1>
-            <p>
-              <strong>
-                <small>Gestiona envios entre locaciones</small>
-              </strong>
-            </p>
-          </header>
-          <div class="d-grid width100 gap-3">
-            <Link href="/login">
-              <button className="btn btn-primary" type="button">
-                Entrar
-              </button>
-            </Link>
-            <Link href="/register">
-              <button className="btn btn-dark" type="button">
-                Registro
-              </button>
-            </Link>
+        <div className="col-12 min80 d-flex flex-column align-items-center justify-content-center">
+          <div className="row width100">
+            <div className="col-md-1 col-lg-1"></div>
+            <div className="col-md-5 col-lg-6" style={{marginBottom: "30px"}}>
+              <header>
+                <h1>Hola! esto es</h1>
+                <h1>My Camion</h1>
+                <p>
+                  <strong>
+                    <small>Gestiona envios entre locaciones</small>
+                  </strong>
+                </p>
+              </header>
+              <div class="d-grid gap-2 col-6 mx-auto">
+                <Link href="/register">
+                  <button className="btn btn-dark" type="button">
+                    Registro
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div className="col-md-5 col-lg-4">
+              <Login />
+            </div>
+            <div className="col-md-1 col-lg-1"></div>
           </div>
         </div>
-        <div className="col-sm-2 col-md-3 col-lg-4"></div>
       </div>
     </BlankLayout>
   );
