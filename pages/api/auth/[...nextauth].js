e1c214893a5026ced2f3066c383ab79dcfe0c261
LeanderShulgin <leandrohornos@gmail.com>
2021-12-02 12:28:08 -0300

    Dejo solo signin con Twitter y Github

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import Providers from "next-auth/providers";
 
 export default NextAuth({
   providers: [
-    Providers.Email({
+/*     Providers.Email({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
         port: process.env.EMAIL_PORT,
@@ -13,18 +13,18 @@ export default NextAuth({
         },
       },
       from: process.env.EMAIL_FROM,
-    }),
-    Providers.Google({
+    }), */
+/*     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    }), */
     Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    Providers.Twitter({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
     }),
   ],
   database: {
---
+++ pages/protected.jsx
@@ -0,0 +1,9 @@
+/* 
+
+PROTECTED PAGE
+--------------
+
+Ese es un ejemplo de una url protegida. Solo puede accederse a ella si se ha iniciado sesión
+
+*/
+
